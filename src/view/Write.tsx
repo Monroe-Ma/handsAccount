ee8be9324fa0966a7d03d5e390133b445be9da9c
mamenglu <2245468037@qq.com>
2022-01-17 11:16:04 +0800

    完成ClassiFication、Output样式

--- src/view/Write.tsx
+++ src/view/Write.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import Layout from 'component/Layout';
+import styled  from 'styled-components';
+const ClassiFication = styled.section`
+  background: #fff;
+  >ul{
+     display: flex;
+     justify-content: space-around;
+     line-height: 20px;
+     padding: 17px 0;
+   }
+`;
+const Tags = styled.section`
+  background: #fff;
+ 
+`;
+const Output = styled.section``
 const Write = () => { 
-
   return (
-   <Layout title = "记一笔">
-    <h2>记一笔</h2>
+  <Layout title = "记一笔">
+      <ClassiFication>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
+      </ClassiFication>
+      <Output>
+        <div><span>￥</span>0.00</div>  
+      </Output>
+      <Tags>
+
+      </Tags>
+
+
+
+      
   </Layout>
   )
-
 }
 export default Write
